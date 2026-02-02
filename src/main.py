56be2eea4d774a84a82d482470478b2a67dd164e
AXRYA7 <aaryabharadwajvr46@gmail.com>
2026-02-02 11:59:50 +0530

    Data Preprocessing + FV Code

--- src/main.py
+++ src/main.py
@@ -1,26 +1,37 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from pyspark.ml.feature import VectorAssembler, StandardScaler
 
 def main():
     spark = SparkSession.builder \
         .appName("FraudDetectionBigData") \
         .getOrCreate()
 
-    
-    df = spark.read.csv(
-        "data/creditcard.csv",
-        header=True,
-        inferSchema=True
+    df = spark.read.csv("data/creditcard.csv", header=True, inferSchema=True)
+
+    # Separate features and label
+    feature_cols = [c for c in df.columns if c != "Class"]
+
+    assembler = VectorAssembler(
+        inputCols=feature_cols,
+        outputCol="features_raw"
     )
 
-    # Basic checks
-    df.printSchema()
-    df.show(5)
+    df_vec = assembler.transform(df)
 
-    # Class distribution (fraud vs non-fraud)
-    df.groupBy("Class").count().show()
+    scaler = StandardScaler(
+        inputCol="features_raw",
+        outputCol="features",
+        withMean=True,
+        withStd=True
+    )
+
+    scaler_model = scaler.fit(df_vec)
+    df_final = scaler_model.transform(df_vec)
+
+    df_final.select("features", "Class").show(5)
 
     spark.stop()
 
 if __name__ == "__main__":
     main()
-
